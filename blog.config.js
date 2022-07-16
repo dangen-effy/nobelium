110bd2ccc94348af3048e0757b1d7c5f085d1732
dangen-effy <dangen.effy@gmail.com>
2022-07-16 12:03:06 +0900

    refactor: 💡 google seo, ga

--- blog.config.js
+++ blog.config.js
@@ -20,19 +20,19 @@ const BLOG = {
   socialLink: '',
   seo: {
     keywords: ['DevOps', 'Kubernetes', 'AWS', 'Cloud Native'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: 'CLRX89W78AJJSG9mprMtpPmsFYjlpGbQ5oDSk5FgLYM' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-JWRXR9GP58' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
